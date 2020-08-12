391daa3c91bf32d107cbddc0a5c57e1668d6f589
Anand <anandgupta887@yahoo.com>
2020-08-12 16:03:01 +0530

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import List from "./List";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setItems([...items, input]);
+    console.log(items);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <input type="text"></input>
-      <button>Add</button>
+      <form>
+        <input onChange={handleChange} value={input} />
+        <button onClick={handleClick}>Add</button>
+      </form>
+      <ul>
+        {items.map((item) => (
+          <List text={item} />
+        ))}
+      </ul>
     </div>
   );
 }
