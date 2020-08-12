e7e425bb0f5bebc9c22317e6ae5120c55f349dee
Anand <anandgupta887@yahoo.com>
2020-08-12 14:33:29 +0530

    initial stage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text"></input>
+      <button>Add</button>
+    </div>
+  );
+}
+
+export default App;
